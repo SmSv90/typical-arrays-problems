be6d16e295a62ca0f5ad16f3b6ab5423df3b61b8
Sviatoslav Smovdyr <48630769+SmSv90@users.noreply.github.com>
2020-09-30 22:15:10 +0300

    feat
    add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
+  if (array !== [] && array != null && array[0] != null) {
+    return Math.min(...array);
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array !== [] && array != null && array[0] != null) {
+    return Math.max(...array);
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array != [] && array != null && array[0] != null) {
+    let sum = 0;
+    for( let i = 0; i < array.length; i++ ){
+    sum += array[i]; 
+}
+  let average = sum/ array.length;
+  return average;
+  }
   return 0;
 }
